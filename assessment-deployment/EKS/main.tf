09282fd90766642b9f9b7fe7349024cd241f4cf6
Cloud User <ec2-user@ip-10-0-2-126.ap-south-1.compute.internal>
2024-06-05 19:16:24 +0000

    made EKS public

--- assessment-deployment/EKS/main.tf
+++ assessment-deployment/EKS/main.tf
@@ -34,7 +34,8 @@ module "eks" {
 
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.24"
-
+  
+  cluster_endpoint_private_access = false
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
